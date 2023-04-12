18f2bf0a12ed7e5b6430093739956c935df23dba
raphael percy <raphaelisango.ir@gmail.com>
2023-04-13 02:59:55 +0300

    1.0.2

--- index.js
+++ index.js
@@ -7,9 +7,3 @@ await client.connect();
 const aString = await client.ping(); // 'PONG'
 const aNumber = await client.hSet("foo", "alfa", "42", "bravo", "23"); // 2
 const aHash = await client.hGetAll("foo"); // { alfa: '42', bravo: '23' }
-
-const listener = (message, channel) => console.log(message, channel);
-
-await client.pSubscribe("channe*", listener);
-
-await client.publish("channel", "message");
