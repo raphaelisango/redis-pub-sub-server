3ee61658fd2a3befdc13d8369013de998db64a88
raphael percy <raphaelisango.ir@gmail.com>
2023-04-13 03:03:06 +0300

    1.0.2

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ await client.connect();
 const aString = await client.ping(); // 'PONG'
 const aNumber = await client.hSet("foo", "alfa", "42", "bravo", "23"); // 2
 const aHash = await client.hGetAll("foo"); // { alfa: '42', bravo: '23' }
+
+console.log(aString);
+console.log(aHash);
