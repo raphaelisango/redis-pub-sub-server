8ba7d3ccff71a1f1fd4b31df337e9f3e3b9acae6
raphael percy <raphaelisango.ir@gmail.com>
2023-06-07 13:23:17 +0300

    add pub2 and sub2

---
+++ pub2.js
@@ -0,0 +1,17 @@
+import { createClient } from "redis";
+
+const client = createClient();
+client.on("error", (err) => console.log("redis Client Error", err));
+await client.connect();
+
+const aString = await client.ping(); // 'PONG'
+const aNumber = await client.hSet("foo", "alfa", "42", "bravo", "23"); // 2
+const aHash = await client.hGetAll("foo"); // { alfa: '42', bravo: '23' }
+
+//const listener = (message, channel) => console.log(message, channel);
+
+// Use sPublish for sharded Pub/Sub:
+await setInterval(() => {
+  client.publish("channel", "message");
+  client.sPublish("channel", "message");
+}, interval);
---
+++ sub2.js
@@ -0,0 +1,15 @@
+import { createClient } from "redis";
+
+const client = createClient();
+client.on("error", (err) => console.log("redis Client Error", err));
+await client.connect();
+
+const aString = await client.ping(); // 'PONG'
+const aNumber = await client.hSet("foo", "alfa", "42", "bravo", "23"); // 2
+const aHash = await client.hGetAll("foo"); // { alfa: '42', bravo: '23' }
+
+const listener = (message, channel) => console.log(message, channel);
+await client.subscribe("channel", listener);
+await client.pSubscribe("channe*", listener);
+// Use sSubscribe for sharded Pub/Sub:
+await client.sSubscribe("channel", listener);
